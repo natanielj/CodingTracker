ab9e2930c98a1ada202d2ba8ba470ab402dca39f
Nataniel Jayaseelan <63678990+natanielj@users.noreply.github.com>
2025-04-13 08:37:06 -0700

    projects works backend not yet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,8 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -28,12 +30,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
--- app/projForm.tsx
+++ app/projForm.tsx
@@ -1,26 +1,77 @@
 import { ThemedText } from '@/components/ThemedText';
 import React, { useState } from 'react';
-import {View, Text, Button, TextInput} from 'react-native';
+import {View, Text, Button, TextInput, Alert} from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
+import main from '../scripts/call-gemini.js';
+import { GoogleGenAI, createUserContent, createPartFromUri } from "@google/genai";
+import { set } from 'react-hook-form';
+
+
+
+const ai = new GoogleGenAI({ apiKey: process.env.EXPO_PUBLIC_GEMINI_API_TEST });
+
 
 export default function ProjectForm() {
-
+    const [projectName, setProjectName] = useState("");
+    const [projectDescription, setProjectDescription] = useState("");
+    const [githubLink, setGithubLink] = useState("");
+    const [currentStatus, setCurrentStatus] = useState("");
+    const [languageUsed, setLanguageUsed] = useState("");
     const [showBrainstorm, setShowBrainstorm] = useState(false);
-
-
+    const [genProjectName, setGenProjectName] = useState("");
+    const [genProjectDescription, setGenProjectDescription] = useState("");
+    const [genLanguageUsed, setGenLanguageUsed] = useState("");
+    
     const handleBrainstorm = () => {
         setShowBrainstorm(prev => !prev);
     }
 
     function BrainstormSection() {
+        const [idea, setIdea] = useState("");
+        const [prompt, setPrompt] = useState("");
+
+        async function handleAddIdea() {
+            // const cache = await ai.caches.create({
+            //     model: modelName,
+            //     config: {
+            //       contents: createUserContent(createPartFromUri(doc.uri, doc.mimeType)),
+            //       systemInstruction: "You are an expert analyzing transcripts.",
+            //     },
+            // });
+            const response = await ai.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: prompt,
+                config: {
+                    systemInstruction: "You are a helpful assistant that brainstorms project ideas for a beginner. Generate only one idea at a time. Output the following ideas in a concise manner. [STRICT] Use only this format to output: 'Project Name, Project Description(DO NOT USE commas in this description as the output needs to be parsed), Suggest Coding Language'. Suggest different ideas for each request.",
+                },
+            });
+            const out = response.text ?? "No content available";
+            const parts = out.split(',');
+            console.log("Response:", out);
+            // Check if we have at least three parts
+            if (parts.length >= 3) {
+                setGenProjectName(parts[0].trim());
+                setGenProjectDescription(parts[1].trim());
+                setGenLanguageUsed(parts[2].trim());
+            } else {
+              console.warn("Unexpected response format:", out);
+            }
+            
+            Alert.alert(genProjectName);
+
+
+        }
+
         return (
             <View style={formStyles.BrainstormSection}>
                 <TextInput
                     style={{ borderWidth: 1, borderColor: '#ccc', padding: 8, marginBottom: 10 }}
-                    placeholder="Brainstorm ideas"
-                    multiline
+                    value={prompt}
+                    onChangeText={setPrompt}
+                    placeholder='Brainstorm a project idea'
                 />
-                <Button title="Add Idea" onPress={() => {}} />
+                <Button title="Add Idea" onPress={() => {handleAddIdea()}} />
+                <Text>{idea}</Text>
             </View>
         );
     }
@@ -37,18 +88,23 @@ export default function ProjectForm() {
                     <Text>Project Name</Text>
                     <TextInput
                         style={{ borderWidth: 1, borderColor: '#ccc', padding: 8, marginBottom: 10 }}
-                        placeholder="Enter project name"
+                        placeholder={(genProjectName!== null) ? genProjectName : "Enter project name"}
+                        value={(genProjectName!== null) ? genProjectName : projectName}
+                        onChangeText={setProjectName}
                     />
                     <Text>Project Description</Text>
                     <TextInput
                         style={{ borderWidth: 1, borderColor: '#ccc', padding: 8, marginBottom: 10 }}
-                        placeholder="Enter project description"
+                        placeholder={(genProjectDescription!== null) ? genProjectDescription : "Enter project description"}
+                        value={(genProjectDescription!== null) ? genProjectDescription : projectDescription}
                         multiline
+                        onChangeText={setProjectDescription}
                     />
                     <Text>Github Link</Text>
                     <TextInput
                         style={{ borderWidth: 1, borderColor: '#ccc', padding: 8, marginBottom: 10 }}
                         placeholder="Enter Github link"
+                        onChangeText={setGithubLink}
                     />
                     <Text>Current Status</Text>
                     <Dropdown
@@ -66,7 +122,9 @@ export default function ProjectForm() {
                     <Text>Language Used</Text>
                     <TextInput
                         style={{ borderWidth: 1, borderColor: '#ccc', padding: 8, marginBottom: 10 }}
-                        placeholder="Enter language used"
+                        placeholder={(genLanguageUsed!== null) ? genLanguageUsed : "Enter coding language used"}
+                        onChangeText={setLanguageUsed}
+                        value={(genLanguageUsed!== null) ? genLanguageUsed : languageUsed}
                     />
                     <Button title="Submit" onPress={() => {}} />
                 </View>
